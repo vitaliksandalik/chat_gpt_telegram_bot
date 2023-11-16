e9e3019191996cf8a5d05106ff2e660eb4ca882f
Vitalii Golovko <vitaliy1337g@gmail.com>
2023-11-16 22:06:12 +0100

    Created message_templates, added language keyboard and some small changes

--- main.py
+++ main.py
@@ -2,62 +2,86 @@ import os
 import asyncio
 import logging
 import sys
-import json
 
 from openai import OpenAI
 
-
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery
 from aiogram.utils.markdown import hbold
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 from dotenv import load_dotenv
 
-load_dotenv()  # This is new, to load the environment variables from .env file
+from message_templates import message_templates
 
+
+load_dotenv()  # This is new, to load the environment variables from .env file
 TOKEN = os.getenv("TOKEN")  # Bot token
 
 dp = Dispatcher()
 client = OpenAI()
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+messages = {}
+user_languages = {}   # Track of user's current language
+
+language_keyboard = InlineKeyboardBuilder()
+language_keyboard.add(types.InlineKeyboardButton(text="English🇬🇧", callback_data='en'),
+                      types.InlineKeyboardButton(text="Український🇺🇦", callback_data='ua'))
 
 
-def show_json(obj):
-    print(json.loads(obj.model_dump_json()))
+@dp.callback_query(F.data.in_({"ua", "en"}))
+async def process_callback(callback: CallbackQuery):
+    user_languages[callback.from_user.id] = callback.data
+    message_template = message_templates[callback.data]['language_confirmation']
+    await callback.message.answer(text=message_template)
 
 
-async def ask_gpt(question: str) -> str:
+@dp.message(Command('language'))
+async def language_cmd(message: types.Message):
+    language = user_languages.get(message.from_user.id, 'en')
+    await message.answer(text=message_templates[language]['language_selection'],
+                         reply_markup=language_keyboard.as_markup())
+
+
+async def ask_gpt(userid) -> str:
     # Function to ask GPT a question and get the response
-    messages = [
-        {"role": "system", "content": 'You answer question about Web  services.'},
-        {"role": "user", "content": question},
-    ]
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=messages,
+        messages=messages[userid],
         temperature=0,
     )
 
     return response.choices[0].message.content
 
 
-@dp.message()
-async def gpt_response_handler(message: types.Message) -> None:
-    # GPT response handler
-    gpt_response = await ask_gpt(message.text)
-    await message.answer(gpt_response)
-
-
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """ This handler receives messages with `/start` command """
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    userid = message.from_user.id
+    messages[userid] = []
+    language = user_languages.get(message.from_user.id, 'en')  # Get the selected language
+    await message.reply(message_templates[language]['start'])  # Retrieve the correct message based on the language
+
+
+@dp.message()
+async def gpt_response_handler(message: Message) -> None:
+    # GPT response handler
+    user_message = message.text
+    userid = message.from_user.id
+    if userid not in messages:
+        messages[userid] = []
+
+    messages[userid].append({"role": "user", "content": user_message})
+    gpt_response = await ask_gpt(userid)
+    await message.answer(gpt_response)
 
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     # And the run events dispatching
     await dp.start_polling(bot)
 
---
+++ message_templates.py
@@ -0,0 +1,26 @@
+message_templates = {
+    'en': {
+        'start': "Hello, I'm bot your GPT assistant. Enter /help",
+        'new_topic': 'Starting a new topic!',
+        'image_prompt': 'Please add a description of the image after the /image command. For example, /image Neon City.',
+        'image_error': 'An error occurred during image generation:',
+        'about': 'This bot is powered by OpenAI GPT-4.',
+        'help': 'You can interact with this bot using commands like /start, /newtopic, /image, /about, and /help.',
+        'language_confirmation': "Language has been set to English.",
+        'language_selection': "Available in 2 languages:",
+        'processing': "Your request is being processed, please wait.",
+        'error': "An error occurred during processing:"
+    },
+    'ua': {
+        'start': "Привіт, я твій GPT помічник. Введіть /help",
+        'new_topic': 'Починаємо нову тему!',
+        'image_error': 'Будь ласка, додайте опис зображення після команди  /image. Наприклад,  /image Неонове місто',
+        'image_error': 'Під час створення зображення сталася помилка',
+        'about': 'Цей бот працює на основі OpenAI GPT-4.',
+        'help': 'Ви можете взаємодіяти з цим ботом за допомогою таких команд, як /start, /newtopic, /image, /about, and /help.',
+        'language_confirmation': "Обрана українська мова",
+        'language_selection': "В наявності 2 мови",
+        'processing': "Ваш запит обробляється, зачекайте.",
+        'error': "Під час обробки сталася помилка:"
+    }
+}
